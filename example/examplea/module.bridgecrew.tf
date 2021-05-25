941196fb1e825c754ee16845815e39cfc117b02e
James Woolfenden <james.woolfenden@gmail.com>
2021-05-25 17:21:50 +0100

    clean

--- example/examplea/module.bridgecrew.tf
+++ example/examplea/module.bridgecrew.tf
@@ -1,6 +1,6 @@
 module "bridgecrew" {
   source           = "../../"
   org_name         = "james"
-  bridgecrew_token = "e1debacc-fb6d-5230-89f4-ec76f383d092"
+  bridgecrew_token = "xxxxxxxx-xxxx-xxxxxxxxx-xxxxxxxxxxxx"
   project_id       = "examplea-304315"
 }
